c1cd8f6b26445e44214e5c770c1dcc833e322ed5
cldnjs <a74417407@gmail.com>
2017-11-07 19:50:02 +0900

    가수, 제목 파싱 구현 완료

--- parsing.js
+++ parsing.js
@@ -5,43 +5,43 @@ const cheerio = require('cheerio');
 /**
  *  실시간 차트를 파싱하여 josn객체로 반환하는 함수 getCurrentChart()
  */
-function getCurrentChart() {
-    var url = 'http://music.naver.com/listen/top100.nhn?domain=TOTAL&duration=1h';
-    var chart = null;
+function getCurrentChart(callback) {
+    var url = 'https://music.bugs.co.kr/chart/track/day/total';
 
     request(url, function (err, response, html) {
         if(err) { throw err; }
 
         var $ = cheerio.load(html);
-        var name = $('td.name').text().trim().replace(/\t+/g, "").split('\n');
-        var artistName = $('a._artist').text().trim().replace(/\t+/g, "").split('\n');
-
-        removeSpaces(name);
-        removeSpaces(artistName);
-        name.splice(0, 3);
-
         var currentChart = new Array();
         var songInfo = new Object();
-        for(var i=0; i<50; i++) {
-            songInfo.title = name[i];
-            songInfo.artist = artistName[i];
+
+        for(var i=0; i<100; i++) {
+            songInfo.title = $('p.title > a').eq(i).text();
+            songInfo.artist = $('p.artist > a:not(.more)').eq(i).text();
             currentChart.push(songInfo);
             songInfo = new Object();
         }
-        chart = currentChart;
-        console.log(chart);
+        callback(currentChart);
     });
 }
 
 function removeSpaces(arr) {
-    for (var index in arr) {
-        if ((arr.indexOf('') !== -1) || (arr.indexOf(' ') !== -1) || (arr.indexOf('  ') !== -1)) {
-            if ((arr[index].length === 0) || (arr[index] === ' ')) {
-                arr.splice(index, 1);
+    if(arr.length > 0) {
+        for (var index in arr) {
+            if ((arr.indexOf('') !== -1) || (arr.indexOf(' ') !== -1) || (arr.indexOf('  ') !== -1)) {
+                if ((arr[index].length === 0) || (arr[index] === ' ')) {
+                    arr.splice(index, 1);
+                }
+            } else {
+                return;
             }
-        } else {
-            return;
         }
+    } else {
+        return;
     }
     removeSpaces(arr);
 }
+
+getCurrentChart((data) => {
+    console.log(data);
+});
