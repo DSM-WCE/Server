30bae99d7234092599bd0383e15f6eed834357ed
cldnjs <a74417407@gmail.com>
2017-11-11 10:44:03 +0900

    DB연동, 스케줄러 등록을 제외한 작업 완료

--- Parsing.py
+++ Parsing.py
@@ -1,10 +1,12 @@
 from bs4 import BeautifulSoup
-from collections import OrderedDict
-import json
+from urllib import request
 import requests
+import os
+import shutil
 
 
 URL = 'https://music.bugs.co.kr/chart/track/day/total'
+PATH = os.getcwd() + '/static/images/'
 
 
 # Scrapping html code
@@ -34,9 +36,43 @@ def get_chart_info():
 
     current_chart = []
     for i in range(0, 100):
-        song_info = OrderedDict()
-        song_info['title'] = title[i]
-        song_info['artist'] = artist[i]
-        current_chart.append(json.dumps(song_info, ensure_ascii=False, indent='\t'))
+        song_info = {
+            'title': title[i],
+            'artist': artist[i]
+        }
+        current_chart.append(song_info)
 
     return current_chart
+
+
+# parse image url and save in list
+def get_image_url():
+    html = get_html(URL)
+    soup = BeautifulSoup(html, 'html.parser')
+    img_url = []
+
+    for image in soup.select('a.thumbnail > img'):
+        if image.has_attr('src'):
+            img_url.append(image.get('src'))
+        else:
+            continue
+    return img_url
+
+
+# download album art in static/images directory
+def download_album_arts():
+    images = get_image_url()
+    for i in range(0, 100):
+        url = images[i]
+        file_name = PATH + str(i + 1) + '.png'
+        request.urlretrieve(url, file_name)
+
+
+# delete all album art
+def delete_album_art():
+    path = os.getcwd() + '/static/images'
+    if os.path.exists(path):
+        shutil.rmtree(path)
+        os.mkdir(path)
+    else:
+        os.mkdir(path)
--- db.py
+++
@@ -1,8 +0,0 @@
-import pymysql
-
-conn = pymysql.connect(host='localhost', user='root',
-                       password='cldnjs2085!', db='wce', charset='utf8')
-
-curs = conn.cursor()
-query = 'desc info'
-curs.execute(query)
